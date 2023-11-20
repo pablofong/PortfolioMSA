bf1897f6c12da59fa55dc13ef65fc2dd0ff3ce7e
Jose Pablo Fong Coronado <63012085+pablofong@users.noreply.github.com>
2023-11-20 22:16:24 +0000

    setup-changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jose Fong",
+  title: "Computer Science and Technology Student",
+  email: "pablo_fong@hotmail.com",
+  gitHub: "pablofong",
+  instagram: "pablofongg",
+  linkedIn: "josé-pablo-fong-coronado-33556b19a",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
@@ -44,7 +44,7 @@ const App = () => {
       <Header />
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
-      <Portfolio />
+      {/* <Portfolio /> */}
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/gradientbluepink.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science and Technology student studying at Tecnologico de Monterrey. I believe that programming is the key to unlocking the potential of the digital age.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -33,9 +33,7 @@ const description =
 const skillsList = [
   "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "User interfaces",
   "Graphic design",
 ];
 
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I love finding smart ways to bring education and technology together. My goal is to make learning easier and more exciting by using simple, cool tech solutions. I believe in making education accessible to everyone, using technology to make a real difference in how we learn and grow.";
 
 const About = () => {
   return (
